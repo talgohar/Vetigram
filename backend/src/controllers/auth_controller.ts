ce03cbf8da450deb7ac4e59c1e02054d71719280
talgohar <talgohar2@gmail.com>
2026-02-21 19:39:25 +0300

    asses controllers

---
+++ backend/src/controllers/auth_controller.ts
@@ -0,0 +1,299 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import userModel, { IUser } from "../models/users_model";
+import { NextFunction, Request, Response } from "express";
+import { Document } from "mongoose";
+import ms from "ms";
+import { OAuth2Client } from "google-auth-library";
+
+const register = async (req: Request, res: Response) => {
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await userModel.create({
+      email: req.body.email,
+      username: req.body.username,
+      password: hashedPassword,
+      isVet: req.body.isVet,
+    });
+    res.status(200).send(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+};
+
+type tTokens = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+const generateToken = (userId: string): tTokens | null => {
+  if (!process.env.TOKEN_SECRET) {
+    return null;
+  }
+  if (!process.env.TOKEN_EXPIRES) {
+    return null;
+  }
+
+  // generate token
+  const random = Math.random().toString();
+  const accessToken = jwt.sign(
+    {
+      _id: userId,
+      random: random,
+    },
+    process.env.TOKEN_SECRET,
+    { expiresIn: process.env.TOKEN_EXPIRES as ms.StringValue }
+  );
+  const refreshToken = jwt.sign(
+    {
+      _id: userId,
+      random: random,
+    },
+    process.env.TOKEN_SECRET,
+    { expiresIn: process.env.TOKEN_EXPIRES as ms.StringValue }
+  );
+  return {
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  };
+};
+const login = async (req: Request, res: Response) => {
+  try {
+    console.log("LOGIN BODY:", req.body);
+    const identifier = req.body.identifier?.trim();
+    const user = await userModel.findOne({
+      $or: [{ username: identifier }, { email: identifier }],
+    });
+
+    if (!identifier) {
+      res.status(400).send("missing identifier");
+      return;
+    }
+    if (!user) {
+      res.status(400).send("wrong username or password");
+      return;
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      res.status(400).send("wrong username or password");
+      return;
+    }
+    if (!process.env.TOKEN_SECRET) {
+      res.status(500).send("Server Error");
+      return;
+    }
+    // generate token
+    const tokens = generateToken(user._id);
+    if (!tokens) {
+      res.status(500).send("Server Error");
+      return;
+    }
+    if (!user.refreshToken) {
+      user.refreshToken = [];
+    }
+    user.refreshToken.push(tokens.refreshToken);
+    await user.save();
+    res.status(200).send({
+      accessToken: tokens.accessToken,
+      refreshToken: tokens.refreshToken,
+      _id: user._id,
+    });
+  } catch (err: any) {
+    res.status(400).send(err);
+    console.log("LOGIN ERROR:", err);
+    res.status(500).send(err?.message || "Server error");
+  }
+  
+};
+
+type tUser = Document<unknown, {}, IUser> &
+  IUser &
+  Required<{
+    _id: string;
+  }> & {
+    __v: number;
+  };
+const verifyRefreshToken = (refreshToken: string | undefined) => {
+  return new Promise<tUser>((resolve, reject) => {
+    //get refresh token from body
+    if (!refreshToken) {
+      reject("fail");
+      return;
+    }
+    //verify token
+    if (!process.env.TOKEN_SECRET) {
+      reject("fail");
+      return;
+    }
+    jwt.verify(
+      refreshToken,
+      process.env.TOKEN_SECRET,
+      async (err: any, payload: any) => {
+        if (err) {
+          reject("fail");
+          return;
+        }
+        //get the user id fromn token
+        const userId = payload._id;
+        try {
+          //get the user form the db
+          const user = await userModel.findById(userId);
+          if (!user) {
+            reject("fail");
+            return;
+          }
+          if (!user.refreshToken || !user.refreshToken.includes(refreshToken)) {
+            user.refreshToken = [];
+            await user.save();
+            reject("fail");
+            return;
+          }
+          const tokens = user.refreshToken!.filter(
+            (token) => token !== refreshToken
+          );
+          user.refreshToken = tokens;
+
+          resolve(user);
+        } catch (err) {
+          reject("fail");
+          return;
+        }
+      }
+    );
+  });
+};
+
+const client = new OAuth2Client();
+const googleSignin = async (req: Request, res: Response) => {
+  const credential = req.body.credential;
+
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: credential,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    const email = payload?.email;
+
+
+    if (!email) {
+      res.status(400).send("missing email");
+      return;
+    }
+
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      res.status(400).send("User already exists");
+      return;
+    }
+
+    const username = email.split("@")[0].toLowerCase();
+
+    const user = await userModel.create({
+      email,
+      username,
+      imgUrl: payload?.picture,
+      password: "google-signin",
+      isVet: false,
+    });
+
+    const tokens = generateToken(user._id);
+    if (!tokens) {
+      res.status(500).send("Server Error");
+      return;
+    }
+
+    res.status(200).send(tokens);
+
+  } catch (err) {
+    res.status(400).send("google register failed");
+  }
+};
+
+
+
+const logout = async (req: Request, res: Response) => {
+  try {
+    const user = await verifyRefreshToken(req.body.refreshToken);
+    await user.save();
+    res.status(200).send("success");
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("fail");
+  }
+};
+
+const refresh = async (req: Request, res: Response) => {
+  try {
+    const user = await verifyRefreshToken(req.body.refreshToken);
+    if (!user) {
+      res.status(400).send("fail");
+      return;
+    }
+    const tokens = generateToken(user._id);
+
+    if (!tokens) {
+      res.status(500).send("Server Error");
+      return;
+    }
+    if (!user.refreshToken) {
+      user.refreshToken = [];
+    }
+    user.refreshToken.push(tokens.refreshToken);
+    await user.save();
+    res.status(200).send({
+      accessToken: tokens.accessToken,
+      refreshToken: tokens.refreshToken,
+      _id: user._id,
+    });
+    //send new token
+  } catch (err) {
+    res.status(400).send("fail");
+  }
+};
+
+type Payload = {
+  _id: string;
+};
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authorization = req.header("authorization");
+  const token = authorization && authorization.split(" ")[1];
+
+  if (!token) {
+    res.status(401).send("Access Denied");
+    return;
+  }
+  if (!process.env.TOKEN_SECRET) {
+    res.status(500).send("Server Error");
+    return;
+  }
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, payload) => {
+    if (err) {
+      res.status(401).send("Access Denied");
+      return;
+    }
+    req.params.userId = (payload as Payload)._id;
+    next();
+  });
+};
+
+export default {
+  register,
+  login,
+  refresh,
+  logout,
+  googleSignin,
+};
