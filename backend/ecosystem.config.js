4ad5c30d90770183b901166b7d2f5157f8af37af
talgohar <talgohar2@gmail.com>
2026-02-25 02:46:38 +0300

    user id

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  apps : [{
-    name   : "vtegram-backend",
-    script : "./dist/app.js",
-    env_production: {
-      NODE_ENV: "production",
-    },
-  }]
-}
+  apps: [
+    {
+      name: "vtegram-backend",
+      script: "./dist/app.js",
+      env_file: "./.env_prod",
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
