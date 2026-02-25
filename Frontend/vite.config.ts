e16714c0dac77a1f30808cc47343f8c1470b3801
Sean Nedorez <Shonedo25@gmail.com>
2026-02-25 03:54:02 +0200

    fix

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4001,
+    host: '0.0.0.0'
+  }
 })
